79a26fc1d71ae579afdc631c1cbe936718f8c468
ALDANA TORRES JONATHAN FABIAN <jaldanato@uninpahu.edu.co>
2021-03-05 11:51:39 -0500

    Implementación de ingreso al aplicativo.

--- TallerMecanico/FrmLogin.Designer.cs
+++ TallerMecanico/FrmLogin.Designer.cs
@@ -33,10 +33,12 @@ namespace TallerMecanico
             this.PctLogin = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.txtLogin = new System.Windows.Forms.TextBox();
-            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.BtnAcLogin = new System.Windows.Forms.Button();
             this.BtnSalLogin = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.ChkVerCont = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.PctLogin)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,7 +56,7 @@ namespace TallerMecanico
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(197, 303);
+            this.label1.Location = new System.Drawing.Point(183, 303);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(71, 20);
             this.label1.TabIndex = 1;
@@ -70,21 +72,22 @@ namespace TallerMecanico
             this.txtLogin.TabIndex = 2;
             this.txtLogin.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // txtContraseña
+            // txtContrasena
             // 
-            this.txtContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtContraseña.Location = new System.Drawing.Point(187, 415);
-            this.txtContraseña.Multiline = true;
-            this.txtContraseña.Name = "txtContraseña";
-            this.txtContraseña.Size = new System.Drawing.Size(235, 38);
-            this.txtContraseña.TabIndex = 3;
-            this.txtContraseña.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtContrasena.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContrasena.Location = new System.Drawing.Point(187, 415);
+            this.txtContrasena.Multiline = true;
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.PasswordChar = '*';
+            this.txtContrasena.Size = new System.Drawing.Size(235, 38);
+            this.txtContrasena.TabIndex = 3;
+            this.txtContrasena.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(197, 392);
+            this.label2.Location = new System.Drawing.Point(183, 392);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(102, 20);
             this.label2.TabIndex = 4;
@@ -99,6 +102,7 @@ namespace TallerMecanico
             this.BtnAcLogin.TabIndex = 5;
             this.BtnAcLogin.Text = "Aceptar";
             this.BtnAcLogin.UseVisualStyleBackColor = true;
+            this.BtnAcLogin.Click += new System.EventHandler(this.BtnAcLogin_Click);
             // 
             // BtnSalLogin
             // 
@@ -111,15 +115,36 @@ namespace TallerMecanico
             this.BtnSalLogin.UseVisualStyleBackColor = true;
             this.BtnSalLogin.Click += new System.EventHandler(this.BtnSalLogin_Click);
             // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(35, 533);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 7;
+            // 
+            // ChkVerCont
+            // 
+            this.ChkVerCont.AutoSize = true;
+            this.ChkVerCont.Location = new System.Drawing.Point(305, 395);
+            this.ChkVerCont.Name = "ChkVerCont";
+            this.ChkVerCont.Size = new System.Drawing.Size(113, 17);
+            this.ChkVerCont.TabIndex = 8;
+            this.ChkVerCont.Text = "Ver Contraseña";
+            this.ChkVerCont.UseVisualStyleBackColor = true;
+            this.ChkVerCont.CheckedChanged += new System.EventHandler(this.ChkVerCont_CheckedChanged);
+            // 
             // FrmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(612, 583);
+            this.Controls.Add(this.ChkVerCont);
+            this.Controls.Add(this.lblResultado);
             this.Controls.Add(this.BtnSalLogin);
             this.Controls.Add(this.BtnAcLogin);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtContrasena);
             this.Controls.Add(this.txtLogin);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.PctLogin);
@@ -138,9 +163,11 @@ namespace TallerMecanico
         private System.Windows.Forms.PictureBox PctLogin;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtLogin;
-        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.TextBox txtContrasena;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button BtnAcLogin;
         private System.Windows.Forms.Button BtnSalLogin;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.CheckBox ChkVerCont;
     }
 }
